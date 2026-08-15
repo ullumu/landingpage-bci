23869eb198b988436eaf69621ab096d616417e6d
ullumu <ullumu18@gmail.com>
2026-08-15 07:35:54 +0700

    update config

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   // Jika di-deploy ke GitHub Pages di subpath /<repo>, ganti nilai di bawah sesuai nama repo
-  basePath: "/landingpage-bci",
-  assetPrefix: "/landingpage-bci",
+  // basePath: "/landingpage-bci",
+  // assetPrefix: "/landingpage-bci",
   env: {
-    NEXT_PUBLIC_BASE_PATH: "/landingpage-bci",
+    // NEXT_PUBLIC_BASE_PATH: "/landingpage-bci",
+    NEXT_PUBLIC_BASE_PATH: "./",
   },
   trailingSlash: true,
   images: { unoptimized: true },
